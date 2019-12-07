4d01026d7a06e806943319681cadcaef58384edf
MyrPasko <myroslavpasko@gmail.com>
2019-12-07 21:42:31 +0200

    Lesson 2 Observer and observable.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Logic outside of component. For some reason does not works.
+// const counterState = observable({
+//   count: 0,
+// });
+//
+// counterState.incrementHandler = function () {
+//   this.count++;
+// };
+//
+// counterState.decrementHandler = function () {
+//   this.count--;
+// };
+
+@observer
+class App extends Component {
+
+// Logic inside component
+  @observable count = 0;
+  countIncrementHandler = () => this.count++;
+  countDecrementHandler = () => this.count--;
+
+  // Logic outside of component. For some reason does not works.
+  // countIncrementHandler = () => { this.props.store.incrementHandler() };
+  // countDecrementHandler = () => { this.props.store.decrementHandler() };
+
+  render() {
+
+    return (
+      <div className="App">
+        <h1>{this.count}</h1>
+        <button onClick={this.countIncrementHandler}>+1</button>
+        <button onClick={this.countDecrementHandler}>-1</button>
+      </div>
+    );
+  }
+
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
