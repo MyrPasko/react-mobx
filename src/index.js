bb6c26a2a8aff3641b7921f80bda506ef8d15162
MyrPasko <myroslavpasko@gmail.com>
2019-12-08 07:32:05 +0200

    Lesson 3 Computed properties and reactions.

--- src/index.js
+++ src/index.js
@@ -2,39 +2,39 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { observable } from 'mobx';
+import { observable, computed } from 'mobx';
 import { observer } from 'mobx-react';
 
-// Logic outside of component. For some reason does not works.
-// const counterState = observable({
-//   count: 0,
-// });
-//
-// counterState.incrementHandler = function () {
-//   this.count++;
-// };
-//
-// counterState.decrementHandler = function () {
-//   this.count--;
-// };
+const nickName = new class UserNickName {
+  @observable firstName = 'Myroslav';
+  @observable age = 32;
+
+  @computed get getNickName() {
+    console.log('[Computed nickName]');
+    return `${this.firstName} - ${this.age}`;
+  }
+}();
+
+nickName.increment = function () {
+  this.age++;
+};
+
+nickName.decrement = function () {
+  this.age--;
+};
 
 @observer
 class App extends Component {
 
-// Logic inside component
-  @observable count = 0;
-  countIncrementHandler = () => this.count++;
-  countDecrementHandler = () => this.count--;
-
-  // Logic outside of component. For some reason does not works.
-  // countIncrementHandler = () => { this.props.store.incrementHandler() };
-  // countDecrementHandler = () => { this.props.store.decrementHandler() };
+  countIncrementHandler = () => this.props.store.increment();
+  countDecrementHandler = () => this.props.store.decrement();
 
   render() {
 
     return (
       <div className="App">
-        <h1>{this.count}</h1>
+        <h1>{this.props.store.getNickName}</h1>
+        <h1>{this.props.store.age}</h1>
         <button onClick={this.countIncrementHandler}>+1</button>
         <button onClick={this.countDecrementHandler}>-1</button>
       </div>
@@ -43,7 +43,8 @@ class App extends Component {
 
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+
+ReactDOM.render(<App store={nickName}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
