f394f1857d2697f28f2339d8692e3a2ad6fc6053
MyrPasko <myroslavpasko@gmail.com>
2019-12-08 14:01:45 +0200

    Lesson 4 Observable Object and Array

--- src/index.js
+++ src/index.js
@@ -2,41 +2,58 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { observable, computed } from 'mobx';
+import { observable } from 'mobx';
 import { observer } from 'mobx-react';
+// import { isComputedValue } from 'mobx/lib/core/computedvalue';
 
-const nickName = new class UserNickName {
-  @observable firstName = 'Myroslav';
-  @observable age = 32;
+// Observable object
+const nickName = observable({
+  firstName: 'Myroslav',
+  age: 32,
 
-  @computed get getNickName() {
+  get getNickName() {
     console.log('[Computed nickName]');
     return `${this.firstName} - ${this.age}`;
+  },
+
+  increment() {
+    this.age++;
+  },
+
+  decrement() {
+    this.age--;
   }
-}();
+});
 
-nickName.increment = function () {
-  this.age++;
-};
-
-nickName.decrement = function () {
-  this.age--;
-};
+// Observable array
+// This remains observable even after render.
+const todos = observable([
+  { text: 'Learn React' },
+  { text: 'Learn MobX' },
+]);
 
 @observer
 class App extends Component {
 
-  countIncrementHandler = () => this.props.store.increment();
-  countDecrementHandler = () => this.props.store.decrement();
+  // countIncrementHandler = () => this.props.store.increment();
+  // countDecrementHandler = () => this.props.store.decrement();
 
   render() {
 
     return (
       <div className="App">
-        <h1>{this.props.store.getNickName}</h1>
-        <h1>{this.props.store.age}</h1>
-        <button onClick={this.countIncrementHandler}>+1</button>
-        <button onClick={this.countDecrementHandler}>-1</button>
+        {/*<h1>{this.props.store.getNickName}</h1>*/}
+        {/*<h1>{this.props.store.age}</h1>*/}
+        {/*<button onClick={this.countIncrementHandler}>+1</button>*/}
+        {/*<button onClick={this.countDecrementHandler}>-1</button>*/}
+
+        <ul>
+          {this.props.store.map((item) => {
+            const { text } = item;
+
+            return <li key={text}>{text}</li>
+          })}
+        </ul>
       </div>
     );
   }
@@ -44,8 +61,10 @@ class App extends Component {
 }
 
 
-ReactDOM.render(<App store={nickName}/>, document.getElementById('root'));
+ReactDOM.render(<App store={todos}/>, document.getElementById('root'));
 
+// "Todos" remains observable even after render.
+todos.push({ text: 'After render' });
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
